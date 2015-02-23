3d07486a66038b7c1205bc3347c187bbd6d8bce4
bruce <bruce951@sjccs142.saintjoe.edu>
2015-02-23 17:18:21 -0500

    //Code for test

---
+++ PhoneDriver.java
@@ -0,0 +1,9 @@
+public class PhoneDriver {
+	public static void main(String [] args) {
+	Phone = myPhone = new Phone();
+	System.out.println(myPhone.toString());
+
+	Phone anotherPhone = new Phone ("Android");
+	System.out.println(myPhone.toString());
+	}
+} 
